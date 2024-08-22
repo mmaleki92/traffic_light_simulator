be142af941644ee423d3722628c317e9f991f62c
Morteza <maleki.morteza92@gmail.com>
2024-08-22 20:27:18 +0330

    fixed tiles not existing

---
+++ sTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="sTiles" tilewidth="12" tileheight="12" tilecount="900" columns="30">
+ <image source="sTiles.png" width="360" height="360"/>
+</tileset>
